12ca5197e052aa9c285787d863afae65b40308c7
francielle <francielledeabreus@gmail.com>
2023-06-13 18:38:46 -0300

    feat: exercice two done

--- m1s04/exercises/exercises_week_2.js
+++ m1s04/exercises/exercises_week_2.js
@@ -28,3 +28,24 @@ const returnMedia = () => {
   result.textContent = `A média é: ${media.toFixed(2)}`;
   result.style.display = "block";
 };
+
+const returnIMC = () => {
+  const result = document.getElementById("result_imc");
+  result.style.display = "none";
+
+  const weight = parseFloat(document.getElementById("weight").value);
+  const height = parseFloat(document.getElementById("height").value);
+
+  const values = [weight, height];
+
+  let imc = 0;
+
+  const allNumbers = values.every((value) => isNumber(value));
+
+  if (allNumbers) {
+    imc = weight / (height * height);
+    result.textContent = `o IMC é: ${imc.toFixed(2)}`;
+    result.style.display = "block";
+  }
+  return;
+};
